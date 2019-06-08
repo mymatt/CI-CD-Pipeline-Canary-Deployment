f5e982724dd0996204ba0a0bf7fe0e987407d045
Matthew Myers <mym3491@yahoo.com.au>
2019-06-08 16:33:20 +1000

    add docker application

--- go-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-envsubst < /tmp/index.html.template > /sites/go/files/templates/index.html
-envsubst < /tmp/main.go.template > /sites/go/files/main.go
-exec /usr/bin/runsvdir /etc/service
--- nginx-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-envsubst < /tmp/nginx.conf.ctmpl.template > /etc/nginx/nginx.conf.ctmpl
-exec /usr/bin/runsvdir /etc/service
